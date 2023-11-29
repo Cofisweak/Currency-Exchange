70ec8d73082afb950554207824852266ec8ef712
cofisweak <cofisweak@gmail.com>
2023-11-29 22:16:48 +0300

    remove init (create) database method

--- src/main/java/org/cofisweak/util/ConnectionManager.java
+++ src/main/java/org/cofisweak/util/ConnectionManager.java
@@ -14,42 +14,10 @@ import java.util.concurrent.BlockingQueue;
 public class ConnectionManager {
     private static final String DB_URL = "db.url";
     private static final int DEFAULT_POOL_SIZE = 5;
-    private static final String CREATE_DATABASE_IF_NOT_EXISTS_SQL = """
-            CREATE TABLE IF NOT EXISTS currencies
-            (
-                id   INTEGER PRIMARY KEY AUTOINCREMENT,
-                code VARCHAR(3) NOT NULL,
-                full_name VARCHAR(70) NOT NULL,
-                sign VARCHAR(5) NOT NULL
-            );
-            CREATE UNIQUE INDEX IF NOT EXISTS currency_code_index
-                ON currencies (code);
-            CREATE TABLE IF NOT EXISTS exchange_rates (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                base_currency_id INTEGER,
-                target_currency_id INTEGER,
-                rate DECIMAL(6),
-                FOREIGN KEY (base_currency_id) REFERENCES currencies(id) ON DELETE CASCADE,
-                FOREIGN KEY (target_currency_id) REFERENCES currencies(id) ON DELETE CASCADE
-            );
-            CREATE UNIQUE INDEX IF NOT EXISTS exchange_pair_index
-                ON exchange_rates (base_currency_id, target_currency_id);""";
     private static BlockingQueue<Connection> connectionPool;
 
     static {
         initPool();
-        initDefaultDatabase();
-    }
-
-    @SneakyThrows
-    private static void initDefaultDatabase() {
-        try (Connection connection = getConnection();
-             PreparedStatement statement = connection.prepareStatement(CREATE_DATABASE_IF_NOT_EXISTS_SQL)) {
-            statement.execute();
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-            throw new DatabaseNotAvailableException("Unable to init database");
-        }
     }
 
     private static Connection openConnection() throws SQLException {
